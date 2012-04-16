2a636f673c99466f00af62339e47a3d55af9464a
yanxin <xin.yan@china-rewards.com>
2012-04-16 17:38:50 +0800

    Separate db patch to several files, every file should contain only one ddl.

--- main/src/db/patches/000010_qqadidas.sql
+++ main/src/db/patches/000010_qqadidas.sql
@@ -1,9 +1,10 @@
-CREATE TABLE QQActivityMember (
+CREATE TABLE QQActivityHistory (
 	id VARCHAR(255) NOT NULL,
 	memberKey VARCHAR(255),
-	giftStatus VARCHAR(255),
-	privilegeStatus VARCHAR(255),
-	sendTime datetime,
+	aType VARCHAR(255),
+	consumeAmt double precision NOT NULL,
+	rebateAmt double precision NOT NULL,
+	posId VARCHAR(255),
 	createdAt datetime,
 	lastModifiedAt datetime,
 	primary key (id)
